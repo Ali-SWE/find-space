249ae78fd510918ee276f25543b7733112d59038
Ali Alsaihati <ali.y.alsaihati@gmail.com>
2024-03-06 15:27:41 +0300

    border colors

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -24,7 +24,7 @@ const Info = ({changeDay, changeBuilding, changeStartTime, changeEndTime, missin
 
               <select name="select-day"
                id="select-day" 
-               className="border-2 h-8 border-secondary focus:border-primary rounded text-slate-400 text-sm md:text-base text-center bg-transparent"
+               className="border-2 h-8  focus:border-primary rounded border-secondary text-black text-sm md:text-base text-center  bg-transparent"
                onChange={e => changeDay(e.target.value)}
                >
                 <option value={today}>Today</option>
@@ -38,7 +38,7 @@ const Info = ({changeDay, changeBuilding, changeStartTime, changeEndTime, missin
 
               <select name="select-building" 
               id="select-building" 
-              className={`border-2 h-8 focus:border-primary rounded  text-slate-400 bg-transparent
+              className={`border-2 h-8 focus:border-primary rounded text-black bg-transparent
               text-sm md:text-base text-center ${missingBuilding? "border-my-red": "border-secondary"}`}
               onChange={e => changeBuilding(e.target.value)}
              >
@@ -48,12 +48,12 @@ const Info = ({changeDay, changeBuilding, changeStartTime, changeEndTime, missin
 
               <input type="time" name="start-time" id="start-time" defaultValue={currentTime}
               className={`border-2 h-8 focus:border-primary rounded bg-transparent
-            text-slate-400 text-sm md:text-base ${timeError? " border-my-red" :"border-secondary"}`} 
+              text-black text-sm md:text-base ${timeError? " border-my-red" :"border-secondary"}`} 
               onChange={e => changeStartTime(e.target.value)}/>
 
               <input type="time" name="end-time" id="end-time"  defaultValue={currentTimePlusOneHour}
               className={`border-2 h-8 focus:border-primary rounded bg-transparent
-              text-slate-400 text-sm md:text-base ${timeError?"border-my-red" :"border-secondary"}`}  
+              text-black text-sm md:text-base ${timeError?"border-my-red" :"border-secondary"}`}  
               onChange={e => changeEndTime(e.target.value)}/>
 
             </div>
